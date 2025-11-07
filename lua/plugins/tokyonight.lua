45f602552d9885c0c280830ebd2fd9c6003a0ab3
Vinicius <vinitrech@gmail.com>
2025-11-07 15:28:08 -0300

    feat: removing background from cursorline

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -7,6 +7,12 @@ return {
       sidebars = "transparent",
       floats = "transparent",
     },
+    on_highlights = function(hl, c)
+      -- Make cursor line background completely transparent
+      hl.CursorLine = { bg = "none" }
+      -- Keep the current line number bright so you still see your position
+      hl.CursorLineNr = { fg = c.warning, bold = true }
+    end,
   },
   config = function(_, opts)
     require("tokyonight").setup(opts)
