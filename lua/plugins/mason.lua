b64476d67deafc5233ab402e4b721ad8e35d42f5
Vinicius <vinitrech@gmail.com>
2025-11-12 07:55:23 -0300

    feat: configured roslyn lsp + error fixes

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+    },
+  },
+}
