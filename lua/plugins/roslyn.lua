c0b56aeb66d1ac803283e5ee20d0316daa5c13fe
Vinicius <vinitrech@gmail.com>
2026-01-08 23:33:17 -0300

    chore: limit removal for roslyn extensions

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -3,7 +3,6 @@ return {
     "seblyng/roslyn.nvim",
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
-    ft = { "cs", "razor" },
     opts = {
       -- your configuration comes here; leave empty for default settings
     },
